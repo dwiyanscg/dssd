cbf3201040dfb64187dcd70ad29b163e730b1ff4
Dwiyan Setia Cahya Gustaman <dwiyanscahyag@gmail.com>
2019-11-24 03:30:56 +0700

    lanjut

--- js/main.js
+++ js/main.js
@@ -6,4 +6,25 @@ $(document).ready(function() {
         $('.animated-icon2').toggleClass('open');
     });
 
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+
 });
